e24f3752cf1619726becca23a7a18741ae891c05
José Erivam <erivamdev@gmail.com>
2024-07-02 21:22:57 -0300

    :bug: fix: add search products server in local file json

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { About } from "./components/about";
 import { Banner } from "./components/banner";
 import { Services } from "./components/services";
 import { Store } from "./components/store";
 
 export function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <main>
       <Banner />
